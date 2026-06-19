8775ec0f3246b0198851d47559337f1ce43e4cf2
Paul Schmidt <github@rookian.dev>
2026-06-19 07:10:09 +0200

    remove nvm stuff from fish config

--- private_dot_config/fish/config.fish
+++ private_dot_config/fish/config.fish
@@ -6,17 +6,9 @@ set -Ux DOCKER_HOST unix://$XDG_RUNTIME_DIR/podman/podman.sock
 set -Ux GOPATH ~/Code
 set -Ux GOPRIVATE "dev.azure.com/finorun/*"
 
-set -Ux NVM_SYMLINK_CURRENT true
-
 if test -d ~/.local/bin
   if not contains -- ~/.local/bin $PATH
     set -p PATH ~/.local/bin
-  end
-end
-
-if test -d ~/.nvm/current/bin
-  if not contains -- ~/.nvm/current/bin $PATH
-    set -p PATH ~/.nvm/current/bin
   end
 end
 
@@ -24,5 +16,4 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-load_nvm > /dev/stderr
 starship init fish | source
