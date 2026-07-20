c6fc5ccfc6e5a97af7d6921161ee38e49f19f2b4
p.schmidt <p.schmidt@shc-group.de>
2026-07-20 15:10:49 +0200

    Update wezterm config with theme switcher

--- private_dot_config/wezterm/wezterm.lua
+++ private_dot_config/wezterm/wezterm.lua
@@ -1,17 +1,32 @@
 local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
--- fonts
-config.font = wezterm.font("Inconsolata Nerd Font", { weight = "Regular" })
-config.font_size = 12
+function scheme_for_appearance(appearance)
+  if appearance:find "Dark" then
+    return "Catppuccin Frappe"
+  else
+    return "Catppuccin Latte"
+  end
+end
 
-config.hide_tab_bar_if_only_one_tab = true
--- config.window_decorations = "RESIZE"
--- config.window_close_confirmation = "NeverPrompt"
+-- fonts
+config.font = wezterm.font("RobotoMono Nerd Font", { weight = "Regular" })
+-- config.font = wezterm.font("Inconsolata Nerd Font", { weight = "Regular" })
+config.font_size = 14
+-- config.font_size = 12
+
+-- config.freetype_load_flags = "NO_HINTING"
+config.freetype_load_target = "Light"
 config.freetype_render_target = "HorizontalLcd"
 
-config.color_scheme = "Catppuccin Frappe"
+config.hide_tab_bar_if_only_one_tab = true
+config.window_decorations = "RESIZE"
+config.window_close_confirmation = "NeverPrompt"
 
-config.default_prog = { '/usr/bin/fish', '-l', '-c', 'tmux' }
+config.color_scheme = scheme_for_appearance(wezterm.gui.get_appearance())
+-- config.color_scheme = "Catppuccin Frappe"
+
+-- why is this commented out? This probably won't work on Linux, when default shell is not fish
+-- config.default_prog = { '/usr/bin/fish', '-l', '-c', 'tmux' }
 
 return config
